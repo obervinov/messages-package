9f6aa56b7d2198c3492de161a41bbd065601ef78
obervinov <49606362+obervinov@users.noreply.github.com>
2023-03-14 19:38:18 +0400

    create project

---
+++ messages/messages.py
@@ -0,0 +1,88 @@
+"""
+This module contains classes and methods
+for assembling and sending telegram messages to the bot.
+"""
+import json
+import math
+import re
+import emoji
+
+
+class Messages:
+    """
+    This class contains methods
+    for assembling and sending telegram messages to the bot.
+    """
+
+    def __init__(
+        self,
+        config_path: str = 'configs/messages.json'
+    ) -> None:
+        """
+        Method for create a new messages instance.
+
+        :param config: The path as a file containing a dictionary with message templates.
+        :type config: str
+        :default config: configs/messages.json
+        """
+        self.config_path = config_path
+        with open(self.config_path, 'r', encoding='UTF-8') as config_json:
+            self.data = json.loads(config_json)
+        config_json.close()
+
+
+    def render_template(
+        self,
+        template_alias: str = None,
+        **kwargs
+    ) -> str:
+        """
+        Method for reading the text from the configuration file.
+
+        :param template_alias: The name of the template key for extracting text from conifg.
+        :type template_alias: str
+        :default template_alias: None
+        :param **kwargs: Passing additional parameters for message assembly.
+        :type **kwargs: dict
+        """
+        try:
+            template = self.data['templates'][template_alias]
+            arguments = []
+            for arg in template['args']:
+                if re.match(r":.*:", arg):
+                    # Rendering emojis
+                    arguments.append(emoji.emojize(arg))
+                else:
+                    # Rendering kwargs variables
+                    arguments.append(kwargs.get(arg))
+            # Building full message
+            return template['text'].format(*arguments)
+        except KeyError:
+            return None
+
+
+    def render_progressbar(
+        self,
+        total_count: int = 0,
+        current_count: int = 0
+    ):
+        """
+        A Method for generating string with a progress bar based
+        on the transmitted statistics data.
+
+        :param total_count: An integer with the total counter value for the progress bar.
+        :type total_count: int
+        :default total_count: 0
+        :param current_count: Counter of the current integer
+            to calculate the percentage of self.total_count.
+        :type current_count: int
+        :default current_count: 0
+        """
+        procentage = math.ceil((current_count / total_count) * 100)
+        progressbar_response = (
+            '\r[',
+            '\u25FE' * int(procentage),
+            '\u25AB' * int((100 - procentage)),
+            f']{str(procentage)}%'
+        )
+        return progressbar_response
---
+++ setup.py
@@ -0,0 +1,41 @@
+"""This module is necessary to distribute and install the written module via pip"""
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf8') as readme:
+    readme_content = readme.read()
+with open('CHANGELOG.md', 'r', encoding='utf8') as changelog:
+    changelog_content = changelog.read()
+
+setup(
+    name='messages',
+    version='1.0.0',
+    license='MIT',
+    description=(
+        "This package helps to easily and quickly generate beautiful messages"
+        "for telegram bots using templates described in json."
+    ),
+    py_modules=["messages"],
+    package_dir={'': 'src'},
+    author='Oleg Bervinov',
+    author_email='obervinov@pm.me',
+    long_description=(f"{readme_content}""\n\n"f"{changelog_content}"),
+    long_description_content_type="text/markdown",
+    url='https://github.com/obervinov/messages-package',
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent'
+    ],
+    keywords=['messages', 'telegram', 'bot', 'emoji'],
+    install_requires=[
+        'emoji==2.2.0'
+    ],
+    dependency_links=[]
+)
